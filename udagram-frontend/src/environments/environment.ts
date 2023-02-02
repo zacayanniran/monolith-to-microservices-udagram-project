dda247ef9cf46a550b34feb49bbaa4bc94c67aa5
Zacchaeus Ayanniran <zacchdipo@gmail.com>
2023-02-02 15:23:13 +0000

    Changes

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'https://1A34E8D6D83FE609F911D5B98A1FD41D.gr7.us-east-1.eks.amazonaws.com:8080/api/v0'
 };
 
 /*
