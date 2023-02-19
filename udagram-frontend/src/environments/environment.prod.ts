3f4fddfe43bf90deea457c823852ce2fc7ec085e
Zacchaeus Ayanniran <zacchdipo@gmail.com>
2023-02-19 16:19:40 +0000

    New image for frontend service

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://ad71b9bc03506499f850f9a9412329de-1622739792.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
